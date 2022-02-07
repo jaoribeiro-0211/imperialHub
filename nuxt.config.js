ffd3fa59510a3c972fcebf0c20fac766628d1b7d
João Vitor Ribeiro <joaotvrodeio@gmail.com>
2022-02-07 09:45:46 -0300

    Meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,84 +1,98 @@
 import colors from "vuetify/es5/util/colors";
 
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: "%s ",
-    title: "ImperialHub",
-    htmlAttrs: {
-      lang: "pt-BR",
-    },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content:
-          " Com a imperialhub voce aumenta seus lucros, disparando mensagens com uma taxa de abertura acima de 95%.",
-      },
-      { name: "format-detection", content: "telephone=no" },
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/ImperialIcon.png" },
-      { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      { rel: "preconnect", href: "https://fonts.gstatic.com" },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:wght@300;400;500;600;700&display=swap",
-      },
-    ],
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@/assets/styleGlobal.css"],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: "~/plugins/vue-scroll-reveal", ssr: false, mode: "client" }],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/vuetify
-    "@nuxtjs/vuetify",
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios",
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
-
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    /*  customVariables: ["~/assets/variables.scss"], */
-    theme: {
-      dark: false,
-      /*  themes: {
-        dark: {
-          primary: "#0070F3",
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        titleTemplate: "%s ",
+        title: "ImperialHub",
+        htmlAttrs: {
+            lang: "pt-BR",
         },
-      }, */
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            {
+                hid: "description",
+                name: "description",
+                content: "ImperialHub - Com a ImperialHub voce aumenta seus lucros, disparando mensagens com uma taxa de abertura acima de 95%.",
+            },
+            {
+                hid: "twitter:title",
+                name: "twitter:title",
+                content: "ImperialHub",
+            },
+            {
+                hid: "twitter:description",
+                name: "twitter:description",
+                content: "ImperialHub - Com a ImperialHub voce aumenta seus lucros, disparando mensagens com uma taxa de abertura acima de 95%.",
+            },
+            {
+                hid: "twitter:url",
+                name: "twitter:url",
+                content: "https://www.imperialhub.com.br/",
+            },
+            { name: "format-detection", content: "telephone=no" },
+        ],
+        link: [
+            { rel: "icon", type: "image/x-icon", href: "/ImperialIcon.png" },
+            { rel: "preconnect", href: "https://fonts.googleapis.com" },
+            { rel: "preconnect", href: "https://fonts.gstatic.com" },
+            {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:wght@300;400;500;600;700&display=swap",
+            },
+        ],
     },
-  },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: [
-    /*  {
-      src: "nuxt.config.js",
-      use: "@nuxtjs/vercel-builder",
-      config: {},
-    }, */
-  ],
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ["@/assets/styleGlobal.css"],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [{ src: "~/plugins/vue-scroll-reveal", ssr: false, mode: "client" }],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/vuetify
+        "@nuxtjs/vuetify",
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        "@nuxtjs/axios",
+    ],
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {},
+
+    // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+    vuetify: {
+        /*  customVariables: ["~/assets/variables.scss"], */
+        theme: {
+            dark: false,
+            /*  themes: {
+                          dark: {
+                            primary: "#0070F3",
+                            accent: colors.grey.darken3,
+                            secondary: colors.amber.darken3,
+                            info: colors.teal.lighten1,
+                            warning: colors.amber.base,
+                            error: colors.deepOrange.accent4,
+                            success: colors.green.accent3,
+                          },
+                        }, */
+        },
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: [
+        /*  {
+                  src: "nuxt.config.js",
+                  use: "@nuxtjs/vercel-builder",
+                  config: {},
+                }, */
+    ],
 };
